7801f6b5d5dd223bc2da03389dec186222b6231d
Haidang124 <haidangluu94@gmail.com>
2022-04-14 02:45:18 +0700

    setup project with cleanArchitecture

---
+++ MyCleanArchitecture.Domain/common/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCleanArchitecture.Domain.common
+{
+    public abstract class BaseEntity<TId> : IEntity<TId>
+    {
+        public TId Id { get; protected set; } = default!;
+
+        public List<DomainEvent> DomainEvents { get; } = new();
+    }
+   
+}
---
+++ MyCleanArchitecture.Domain/common/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCleanArchitecture.Domain.common
+{
+    public interface IEntity
+    {
+        List<DomainEvent> DomainEvents { get; }
+    }
+
+    public interface IEntity<TId> : IEntity
+    {
+        TId Id { get; }
+    }
+}
