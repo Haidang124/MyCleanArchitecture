f9408cd25360227acc1bf84536f9b3d9bae5ba1b
Haidang124 <haidangluu94@gmail.com>
2022-04-24 17:09:47 +0700

    add DI & middlewares

--- src/Infrastructure.Data/Persistence/Startup.cs
+++ src/Infrastructure.Data/Persistence/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Data.Persistence.ConnectDatabase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,9 +15,18 @@ namespace Infrastructure.Data.Persistence
     {
         internal static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
         {
+            var databaseSettings = config.GetSection(nameof(DatabaseSettings)).Get<DatabaseSettings>();
+            // string? coreConnectionString = databaseSettings.ConnectionString;
             const string coreConnectionString = "server=localhost;userid=root;password=haidang124;database=Ecommerce";
+            // string? coreConnectionString = "server=localhost;userid=root;password=haidang124;database=Ecommerce";
+            // if (string.IsNullOrEmpty(coreConnectionString))
+            // {
+            //     throw new InvalidOperationException("DB ConnectionString is not configured.");
+            // }
             // return services.AddDbContext<EcommerceDbContext>(options => options.UseSqlServer(coreConnectionString));
-            return services.AddDbContext<EcommerceDbContext>(options =>
+            return services
+            .Configure<DatabaseSettings>(config.GetSection(nameof(DatabaseSettings)))
+            .AddDbContext<EcommerceDbContext>(options =>
                 options.UseMySql(
                     coreConnectionString,
                     ServerVersion.AutoDetect(coreConnectionString)
